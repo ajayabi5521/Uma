bb1d957c783174b45060cb47fd24033149716253
ajayabi5521 <ajayabi5521@gmail.com>
2025-06-13 20:19:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Basic confetti effect
+const confettiContainer = document.querySelector('.confetti');
+
+for (let i = 0; i < 100; i++) {
+    const confetti = document.createElement('div');
+    confetti.classList.add('confetti-piece');
+    confetti.style.left = Math.random() * 100 + 'vw';
+    confetti.style.animationDuration = (Math.random() * 3 + 2) + 's';
+    confettiContainer.appendChild(confetti);
+}
